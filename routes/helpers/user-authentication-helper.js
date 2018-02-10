17f9a26a1960cd5fd3eea9019be81ee10575a873
Emily Gao <emily.s.gao@gmail.com>
2018-02-10 15:05:09 -0700

    replace with user helper file

--- routes/helpers/user-authentication-helper.js
+++
@@ -1,9 +0,0 @@
-module.exports = function isUserOrganizer(event, req) {
-
-  if (!req.session || !req.session.user_id || (req.session.user_id !== event.organizer_id)) {
-    return false;
-  } else {
-    return true;
-  }
-
-};
