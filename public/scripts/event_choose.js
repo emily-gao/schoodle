c5ee1a51e170926ede66decba9690326fe6f9607
Joel Tannas <jtannas@gmail.com>
2018-02-10 10:58:18 -0700

    add first pass at linking choice buttons to data

--- public/scripts/event_choose.js
+++ public/scripts/event_choose.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-  $('.event-choice').on('click', function (event) {
+
+  function refreshChoices($element) {
+    $('eventChoices').data('current-choice', $element.data('event-option-id'));
     $('.event-choice').removeClass('btn-success').addClass('btn-danger');
     $('.event-choice').find('i.yes').hide();
     $('.event-choice').find('i.no').show();
 
-    $(this).addClass('btn-success').removeClass('btn-danger');
-    $(this).find('i.yes').show();
-    $(this).find('i.no').hide();
-  });
+    $element.addClass('btn-success').removeClass('btn-danger');
+    $element.find('i.yes').show();
+    $element.find('i.no').hide();
+  }
+
+  $('.event-choice').on('click', function(event) { refreshChoices($(this)); });
+  var currentChoiceId = $('eventChoices').data('current-choice');
+  if (currentChoiceId) {
+    var currentChoiceButton = $('.event-choice[data-event-option-id=' + currentChoiceId + ']');
+    refreshChoices(currentChoiceButton);
+  }
 });
