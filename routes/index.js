52b1762b8e0ead9650d78aaf4fc1538d495c1479
Emily Gao <emily.s.gao@gmail.com>
2018-02-10 09:48:12 -0700

    add get event details route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 "use strict";
 
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+const request = require('request-promise');
+const userAuthenticationHelper = require('./user-authentication-helper');
+
+function internalApiCall(table, queryParams, method = 'GET') {
+  return request({
+    url: `http://localhost:${PORT}/api/${table}`,
+    qs: queryParams,
+    method: method,
+    json: true
+  });
+}
+
+
 function addClientRoutes(router, knex) {
+
   router.get('/', (request, response) => {
     response.render('index');
   });
+
+  router.get('/events/:id', (req, res) => {
+    const eventsQueryParams = { id: req.params.id };
+    const eventOptionsQueryParams = { event_id: req.params.id };
+
+    Promise.all([
+      internalApiCall('events', eventsQueryParams),
+      internalApiCall('event_options', eventOptionsQueryParams)
+    ]).then(([event, event_options]) => {
+
+      const isUserOrganizer = userAuthenticationHelper(event, req);
+      const templateVars = {
+        event: event[0],
+        event_options: event_options
+      };
+
+      if (isUserOrganizer) {
+        res.render('event-organizer', templateVars);
+      } else {
+        res.render('event-guest', templateVars);
+      }
+    });
+
+  });
+
 }
 
 module.exports = addClientRoutes;
