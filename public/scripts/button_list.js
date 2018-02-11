3b92490640e83d35e5c5ed8ad68d877d474f15ce
Emily Gao <emily.s.gao@gmail.com>
2018-02-11 10:48:41 -0700

    add remove button icon

--- public/scripts/button_list.js
+++ public/scripts/button_list.js
@@ -1,20 +1,22 @@
-$(function() {
-  var maxFields = 5;
-  var wrapper = $(".input-fields-wrap");
-  var addButton = $(".add-field-button");
-
-  var currentBoxes = 1;
-  $(addButton).click(function(event) {
-    event.preventDefault();
-    if (currentBoxes < maxFields) {
-      currentBoxes++;
-      $(wrapper).append('<div><input class="options" type="text" name="option" required/><a href="#" class="remove_field">Remove</a></div>');
-    }
-  });
-
-  $(wrapper).on("click", ".remove_field", function(event) {
-    event.preventDefault();
-    $(this).parent("div").remove();
-    currentBoxes--;
+$(document).ready(function() {
+  $(function() {
+    var maxFields = 5;
+    var wrapper = $(".input-fields-wrap");
+    var addButton = $(".add-field-button");
+    
+    var currentBoxes = 1;
+    $(addButton).click(function(event) {
+      event.preventDefault();
+      if (currentBoxes < maxFields) {
+        currentBoxes++;
+        $(this).before('<div><input class="options" type="text" name="option" required/><a class="remove-field btn-floating btn-small waves-effect waves-light green"><i class="material-icons">remove</i></a></div>');
+      }
+    });
+    
+    $(wrapper).on("click", ".remove_field", function(event) {
+      event.preventDefault();
+      $(this).parent("div").remove();
+      currentBoxes--;
+    });
   });
 });
