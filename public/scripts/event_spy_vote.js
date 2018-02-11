8e6c5cad2c8bd4cc25b66d4e70498049c7cea863
Joel Tannas <jtannas@gmail.com>
2018-02-11 05:28:28 -0700

    first pass at styling spy tab

---
+++ public/scripts/event_spy_vote.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//   $('.event-vote').on('click', function (event) {
+//     $(this).toggleClass('btn-success btn-danger');
+//     $(this).find('i.yes').toggle();
+//     $(this).find('i.no').toggle();
+//   });
+// });
