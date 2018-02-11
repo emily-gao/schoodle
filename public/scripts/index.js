983212cfb20131072dbee9cb1aea7f5d5e5613eb
Emily Gao <emily.s.gao@gmail.com>
2018-02-11 08:26:24 -0700

    fix bug: 2 returns after GET /SESSION request are diff data types

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,59 +1,64 @@
 $(function() {
-
+  
   // On load
   $.ajax({
     method: "GET",
     url: "/session",
     success: function(user) {
       if (!user) { return; }
-
+      
       $('.new-schoodle-form')
-        .find("[name='username']")
-        .val(user.username)
-        .prop('disabled', true);
-
+      .find("[name='username']")
+      .val(user.username)
+      .prop('disabled', true);
+      
       $('.new-schoodle-form')
-        .find("[name='email']")
-        .val(user.email)
-        .prop('disabled', true);
+      .find("[name='email']")
+      .val(user.email)
+      .prop('disabled', true);
     }
   });
-
+  
   // post new event info to the database in stages
   // tables by order: users, events, event_options
-
+  
   $('.new-schoodle-form').on('submit', function(event) {
     event.preventDefault();
-
+    
     $.ajax({
       method: "GET",
       url: "/session"
     }).then(function(user) {
-      if (user) { return user; }
-      return $.ajax({
-        method: "POST",
-        url: "/api/users",
-        data: $("[name='username'], [name='email']").serialize()
-      }).then(function(userIds) {
-        return $.ajax({
-          method: 'POST',
-          url: '/register',
-          data: 'user_id=' + userIds[0]
-        });
-      }).then(function(user) {
+      if (user) { 
+        var userId = [];
+        userId.push(user.id);
+        return userId; 
+      } else {
         return $.ajax({
           method: "POST",
-          url: "/api/events",
-          data: $("[name='event_name'], [name='description']").serialize() + '&organizer_id=' + user.id
+          url: "/api/users",
+          data: $("[name='username'], [name='email']").serialize()
         });
-      }).then(function(eventId) {
-        var options = $('.options').serialize().split('&');
-        options.forEach(function(option) {
-          $.ajax({
-            method: "POST",
-            url: "/api/event_options",
-            data: option + '&event_id=' + eventId
-          });
+      }
+    }).then(function(userIds) {
+      return $.ajax({
+        method: 'POST',
+        url: '/register',
+        data: 'user_id=' + userIds[0]
+      });
+    }).then(function(user) {
+      return $.ajax({
+        method: "POST",
+        url: "/api/events",
+        data: $("[name='event_name'], [name='description']").serialize() + '&organizer_id=' + user.id
+      });
+    }).then(function(eventId) {
+      var options = $('.options').serialize().split('&');
+      options.forEach(function(option) {
+        $.ajax({
+          method: "POST",
+          url: "/api/event_options",
+          data: option + '&event_id=' + eventId
         });
       });
     });
