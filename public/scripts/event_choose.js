bf9aea3f5830f33d03396c13f400a23c7043923f
Joel Tannas <jtannas@gmail.com>
2018-02-11 14:05:44 -0700

    link final decision interface to API

--- public/scripts/event_choose.js
+++ public/scripts/event_choose.js
@@ -1,28 +1,45 @@
 $(document).ready(function() {
+  var flash = generateFlashFunction($('#appModal'));
+  var container = $('#eventChoices');
+  var choices = $('.event-choice');
+  var eventId = container.data('event-id');
+  var storedChoice = container.data('stored-choice');
+
+  function currentChoice () {
+    return choices.find('i.yes:visible').parent().data('event-option-id');
+  }
+
 
   function refreshChoices($element) {
     var isAlreadyYes = $element.hasClass('btn-success');
 
-    $('eventChoices').data('current-choice', $element.data('event-option-id'));
-    $('.event-choice').removeClass('btn-success').addClass('btn-danger');
-    $('.event-choice').find('i.yes').hide();
-    $('.event-choice').find('i.no').show();
+    choices.removeClass('btn-success').addClass('btn-danger');
+    choices.find('i.yes').hide();
+    choices.find('i.no').show();
 
     $element.toggleClass('btn-success', !isAlreadyYes).toggleClass('btn-danger', isAlreadyYes);
     $element.find('i.yes').toggle(!isAlreadyYes);
     $element.find('i.no').toggle(isAlreadyYes);
   }
 
-  $('.event-choice').on('click', function(event) { refreshChoices($(this)); });
-  var currentChoiceId = $('eventChoices').data('current-choice');
-  if (currentChoiceId) {
-    var currentChoiceButton = $('.event-choice[data-event-option-id=' + currentChoiceId + ']');
+  choices.on('click', function(event) { refreshChoices($(this)); });
+
+  if (storedChoice !== "none") {
+    var currentChoiceButton = choices.filter('[data-event-option-id=' + storedChoice + ']');
     refreshChoices(currentChoiceButton);
   }
 
-  var flash = generateFlashFunction($('#appModal'));
   $('#notify').on('click', function(event) {
     event.preventDefault();
-    flash('Message: Notifying Guests', 'Notification Title');
+    if (currentChoice()){
+      $.ajax({
+        method: 'patch',
+        url: '/api/events/' + eventId,
+        data: $.param({'final_decision_id': currentChoice()}),
+        success: flash('Choice saved! The invitations should be going out soon', 'Choice Made')
+      });
+    } else {
+      flash('Please make a choice notifying guests', 'Oops', function(event) { return; });
+    }
   });
 });
