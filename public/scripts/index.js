17153fb79db0d702883942cb5977bab7a7050d92
Joel Tannas <jtannas@gmail.com>
2018-02-11 17:51:12 -0700

    add new url flash & social sharing

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,38 +1,49 @@
 $(function() {
-  
+
   // On load
   $.ajax({
     method: "GET",
     url: "/session",
     success: function(user) {
       if (!user) { return; }
-      
+
       $('.new-schoodle-form')
-      .find("[name='username']")
-      .val(user.username)
-      .prop('disabled', true);
-      
+        .find("[name='username']")
+        .val(user.username)
+        .prop('disabled', true);
+
       $('.new-schoodle-form')
-      .find("[name='email']")
-      .val(user.email)
-      .prop('disabled', true);
+        .find("[name='email']")
+        .val(user.email)
+        .prop('disabled', true);
     }
   });
-  
+
+  function flashNewUrl(url) {
+    var localUrl = window.location.href + 'events/' + url;
+    $('#new-url').attr('href', localUrl).text(localUrl);
+    $('.social').each(function() {
+      $(this).attr("href", $(this).attr("href") + encodeURIComponent(localUrl));
+    });
+    $('#newEventModal').on('hidden.bs.modal', function(event) {
+      window.location.replace(localUrl);
+    }).modal();
+  }
+
   // post new event info to the database in stages
   // tables by order: users, events, event_options
-  
+
   $('.new-schoodle-form').on('submit', function(event) {
     event.preventDefault();
-    
+
     $.ajax({
       method: "GET",
       url: "/session"
     }).then(function(user) {
-      if (user) { 
+      if (user) {
         var userId = [];
         userId.push(user.id);
-        return userId; 
+        return userId;
       } else {
         return $.ajax({
           method: "POST",
@@ -53,6 +64,13 @@ $(function() {
         data: $("[name='event_name'], [name='description']").serialize() + '&organizer_id=' + user.id
       });
     }).then(function(eventId) {
+      $.ajax({
+        method: "GET",
+        url: "/api/events/" + eventId
+      }).then(function(event) {
+        flashNewUrl(event[0].url);
+      });
+
       var options = $('.options').serialize().split('&');
       options.forEach(function(option) {
         $.ajax({
@@ -62,5 +80,6 @@ $(function() {
         });
       });
     });
+
   });
 });
